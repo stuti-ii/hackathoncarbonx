11275110ccc242973109de7ae0ad3e0009596fc2
stuti-ii <stutidhungana@gmail.com>
2026-06-13 04:15:30 +0545

    backend ready for deployment

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.config(default="sqlite3:///db.sqlite3")
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
